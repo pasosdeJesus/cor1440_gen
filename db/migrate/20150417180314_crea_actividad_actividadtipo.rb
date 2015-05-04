0c8c20b1d2f065ce1a4a92a41622ce1e41e52254
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-05-03 20:26:38 -0500

    sintaxis

--- db/migrate/20150417180314_crea_actividad_actividadtipo.rb
+++ db/migrate/20150417180314_crea_actividad_actividadtipo.rb
@@ -2,7 +2,7 @@ class CreaActividadActividadtipo < ActiveRecord::Migration
   def up
     execute <<-SQL
         CREATE TABLE cor1440_gen_actividad_actividadtipo (
-            actividad_id    INTEGER REFERENCES cor1440_gen_actividad(id)
+            actividad_id    INTEGER REFERENCES cor1440_gen_actividad(id),
             actividadtipo_id    INTEGER REFERENCES cor1440_gen_actividadtipo(id),
         );
     SQL
