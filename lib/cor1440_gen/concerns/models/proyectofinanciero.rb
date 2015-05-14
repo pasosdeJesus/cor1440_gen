2a1bd73c6538992e5abc303779cc49b7b0e1731d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-05-14 11:20:49 -0500

    Presenta mejor listado y detalles de proyecto financiero y actividades

--- lib/cor1440_gen/concerns/models/proyectofinanciero.rb
+++ lib/cor1440_gen/concerns/models/proyectofinanciero.rb
@@ -21,7 +21,32 @@ module Cor1440Gen
 
           validates :nombre, presence: true, allow_blank: false
           validates :fechacreacion, presence: true, allow_blank: false
+
+          def presenta(atr)
+            case atr.to_s
+            when "financiador_id" 
+              Cor1440Gen::Financiador.find(self[atr]).nombre
+            when "responsable_id"
+              ::Usuario.find(self[atr]).nusuario
+            when "{:proyecto_ids=>[]}"
+              self.proyecto.map { |i| i.nombre }
+            else
+              self[atr].to_s
+            end
+          end
+
         end
+        
+        class_methods do
+          def human_attribute_name(atr)
+            if (atr.to_s == "{:proyecto_ids=>[]}")
+              "Proyectos"
+            else
+              super(atr)
+            end
+          end
+        end
+
       end
     end
   end
