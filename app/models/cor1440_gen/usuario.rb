c2937954376b88caa02a7e4b2a5fcc345639d78b
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-06-11 19:13:45 -0500

    Nuevo modelo de usuario de sip

---
+++ app/models/cor1440_gen/usuario.rb
@@ -0,0 +1,47 @@
+# encoding: UTF-8
+
+module Cor1440Gen
+  class Usuario < Sip::Usuario
+
+    belongs_to :oficina, class_name: 'Sip::Oficina',
+      foreign_key: "oficina_id", validate: true
+
+    has_many :proyectofinanciero, 
+      class_name: 'Cor1440Gen::Proyectofinanciero',
+      foreign_key: 'responsable_id',
+      dependent: :delete_all
+    has_many :actividad_usuario, dependent: :delete_all,
+      class_name: 'Cor1440Gen::ActividadUsuario',
+      foreign_key: 'usuario_id'
+    has_many :actividad, through: :actividad_usuario,
+      class_name: 'Cor1440Gen::Actividad'
+
+    validates_length_of :nusuario, maximum: 15
+
+    validate :rol_usuario
+    def rol_usuario
+      if oficina && (rol == Ability::ROLADMIN ||
+                     rol == Ability::ROLINV || 
+                     rol == Ability::ROLDIR)
+        errors.add(:oficina, "Oficina debe estar en blanco para el rol elegido")
+      end
+      if !oficina && rol != Ability::ROLADMIN && rol != Ability::ROLINV && 
+        rol != Ability::ROLDIR
+        errors.add(:oficina, "El rol elegido debe tener oficina")
+      end
+      #    if (etiqueta.count == 0 && rol == Ability::ROLINV) 
+      #      errors.add(:etiqueta, "El rol invitado debe tener etiquetas compartir")
+      #    end
+      #if (etiqueta.count != 0 && rol != Ability::ROLINV) 
+      #  errors.add(:etiqueta, "El rol elegido no requiere etiquetas de compartir")
+      #end
+      if (!current_usuario.nil? && current_usuario.rol == Ability::ROLCOOR)
+        if (oficina.nil? || 
+            oficina.id != current_usuario.oficina_id)
+          errors.add(:oficina, "Solo puede editar usuarios de su oficina")
+        end
+      end
+    end
+
+  end
+end
--- app/models/usuario.rb
+++
@@ -1,7 +0,0 @@
-# encoding: UTF-8
-
-require 'cor1440_gen/concerns/models/usuario'
-
-class Usuario < ActiveRecord::Base
-  include Cor1440Gen::Concerns::Models::Usuario
-end
