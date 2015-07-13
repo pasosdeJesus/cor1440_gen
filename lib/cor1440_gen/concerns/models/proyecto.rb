d8d9c8db636638982e8192e365fd9d5ef0b7a182
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-07-12 21:20:13 -0600

    Actualiza y errorcito

--- lib/cor1440_gen/concerns/models/proyecto.rb
+++ lib/cor1440_gen/concerns/models/proyecto.rb
@@ -9,7 +9,7 @@ module Cor1440Gen
 
         included do
 
-          has_many :proyecto_proyetofinanciero, dependent: :delete_all,
+          has_many :proyecto_proyectofinanciero, dependent: :delete_all,
             class_name: 'Cor1440Gen::ProyectoProyectofinanciero',
             foreign_key: 'proyecto_id'
           has_many :proyectofinanciero, through: :proyecto_proyectofinanciero,
