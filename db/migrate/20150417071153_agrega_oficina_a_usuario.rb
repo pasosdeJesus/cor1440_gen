94a20a368e72f3728fbeed99daba4a815df16d87
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-08-26 17:31:14 -0500

    Mejora migracion

--- db/migrate/20150417071153_agrega_oficina_a_usuario.rb
+++ db/migrate/20150417071153_agrega_oficina_a_usuario.rb
@@ -1,7 +1,13 @@
 class AgregaOficinaAUsuario < ActiveRecord::Migration
-  def change
-    add_column :usuario, :oficina_id, :integer, 
-      references: 'sip_oficina'
-    add_foreign_key :usuario, :sip_oficina, column: :oficina_id
+  def up
+    if !column_exists? :usuario, :oficina_id
+      add_column :usuario, :oficina_id, :integer, 
+        references: 'sip_oficina'
+      add_foreign_key :usuario, :sip_oficina, column: :oficina_id
+    end
+  end
+
+  def down
+    drop_column :usuario, :oficina_id
   end
 end
