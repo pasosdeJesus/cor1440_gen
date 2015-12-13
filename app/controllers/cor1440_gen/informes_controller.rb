c53224afbcd71fcc47b8fab0c93c87d9fa373df3
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-12-13 06:17:44 -0500

    agrega _otros_filtros a informes

--- app/controllers/cor1440_gen/informes_controller.rb
+++ app/controllers/cor1440_gen/informes_controller.rb
@@ -4,5 +4,6 @@ require_dependency "cor1440_gen/concerns/controllers/informes_controller"
 module Cor1440Gen
   class InformesController < ApplicationController
     include Cor1440Gen::Concerns::Controllers::InformesController
+
   end
 end
