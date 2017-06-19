d357fb74238b0478427f413c18d943f999182c8c
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-06-19 09:12:13 -0500

    Campos de seleccion sencilla y multiple mas usables en formulario de actividades usando chosen

--- app/models/cor1440_gen/proyectofinanciero.rb
+++ app/models/cor1440_gen/proyectofinanciero.rb
@@ -4,6 +4,6 @@ require 'cor1440_gen/concerns/models/proyectofinanciero'
 
 module Cor1440Gen
   class Proyectofinanciero < ActiveRecord::Base
-        include Cor1440Gen::Concerns::Models::Proyectofinanciero
+    include Cor1440Gen::Concerns::Models::Proyectofinanciero
   end
 end
