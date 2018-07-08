1c371aa4821fa40e25d61db96c29e2aa6b1fde80
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-07-08 17:07:02 -0500

    controlador de tipo de actividad generico

---
+++ lib/cor1440_gen/concerns/controllers/actividadtipos_controller.rb
@@ -0,0 +1,58 @@
+# encoding: UTF-8
+
+module Cor1440Gen
+  module Concerns
+    module Controllers
+      module ActividadtiposController 
+        extend ActiveSupport::Concern
+
+        included do
+          include ActionView::Helpers::AssetUrlHelper
+
+          before_action :set_actividadtipo, only: [:show, :edit, :update, :destroy]
+          load_and_authorize_resource class: Cor1440Gen::Actividadtipo
+
+          def clase 
+            "Cor1440Gen::Actividadtipo"
+          end
+
+          def set_actividadtipo
+            @basica = Cor1440Gen::Actividadtipo.find(params[:id])
+          end
+
+          def atributos_index
+            ["id", 
+             "nombre", 
+             "observaciones", 
+             "campoact",
+             "fechacreacion_localizada", 
+             "fechadeshabilitacion_localizada"
+            ]
+          end
+
+          def new
+            @registro = clase.constantize.new
+            @registro.nombre = 'A'
+            @registro.fechacreacion = Date.today
+            @registro.save!(validate: false)
+            redirect_to cor1440_gen.edit_admin_actividadtipo_path(@registro)
+          end
+
+          def genclase
+            'M'
+          end
+
+          def actividadtipo_params
+            params.require(:actividadtipo).permit(
+              *atributos_form + [:campoact_attributes => [
+                :id, :nombrecampo, :tipo, :ayudauso, :_destroy ]
+              ])
+          end
+
+        end
+
+      end
+    end
+  end
+end
+
