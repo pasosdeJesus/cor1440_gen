7fa31f20579742b167f83b5c1945d1d583127a49
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-08-08 06:42:10 -0500

    Agregado flotante como posible tipo de campo dinamico de tipo de actividad

--- app/helpers/cor1440_gen/application_helper.rb
+++ app/helpers/cor1440_gen/application_helper.rb
@@ -9,6 +9,8 @@ module Cor1440Gen
     TIPO_CAMPODIN = [ ['Texto', 1],
                       ['Texto largo', 2],
                       ['Entero', 3],
-                      ['Booleano', 4] ]
+                      ['Booleano', 4],
+                      ['Flotante', 5]
+    ]
   end
 end
