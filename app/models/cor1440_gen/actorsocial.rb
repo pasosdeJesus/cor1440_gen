eafaf66d39ac2e2534e73e452ef9efde662b86ea
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-08-13 16:48:49 -0500

    actorsocial en sip

--- app/models/cor1440_gen/actorsocial.rb
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-
-require 'cor1440_gen/concerns/models/actorsocial'
-
-module Cor1440Gen
-  class Actorsocial < ActiveRecord::Base
-    include Cor1440Gen::Concerns::Models::Actorsocial
-  end
-end
