41c078a6725493615df4cc4f92422f304dad9ea8
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-01-31 07:06:13 -0500

    Actualiza

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -1684,8 +1684,7 @@ CREATE TABLE public.sip_actorsocial (
     pais_id integer,
     web character varying(500),
     created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL,
-    fechadeshabilitacion date
+    updated_at timestamp without time zone NOT NULL
 );
 
 
