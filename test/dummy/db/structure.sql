1226c0c4ebd23f744ad07abc61f73be6d662bb7a
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-02-06 02:18:07 -0500

    Actualiza

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -4402,7 +4402,7 @@ ALTER TABLE ONLY public.usuario
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO public, pg_catalog;
+SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
 ('20131128151014'),
