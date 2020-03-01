187e682e364012245e808016857c8e60f53ada33
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-01 17:33:59 -0500

    Mejora migracion

--- db/migrate/20200229005951_fehayrespnonula.rb
+++ db/migrate/20200229005951_fehayrespnonula.rb
@@ -1,6 +1,14 @@
 class Fehayrespnonula < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    execute <<-SQL
+      UPDATE cor1440_gen_actividad SET fecha='2020-12-31' where fecha IS NULL;
+      UPDATE cor1440_gen_actividad SET usuario_id=1 where usuario_id IS NULL;
+    SQL
     change_column_null :cor1440_gen_actividad, :fecha, false
     change_column_null :cor1440_gen_actividad, :usuario_id, false
+  end
+  def down
+    change_column_null :cor1440_gen_actividad, :fecha, true
+    change_column_null :cor1440_gen_actividad, :usuario_id, true
   end
 end
