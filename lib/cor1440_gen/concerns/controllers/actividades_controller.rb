7b1eb3844f2e9864b44f7da297fd73eb37328c1b
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-22 18:19:10 -0500

    Arregla filtro

--- lib/cor1440_gen/concerns/controllers/actividades_controller.rb
+++ lib/cor1440_gen/concerns/controllers/actividades_controller.rb
@@ -305,7 +305,7 @@ module Cor1440Gen
             if params[:filtro]
               if params[:filtro]['oficina_id']
                 @contar_actividad = @contar_actividad.where(
-                  'cor1440_gen_actividad.oficina_id >= ?', @contar_ofi)
+                  'cor1440_gen_actividad.oficina_id = ?', @contar_ofi)
               end
             end
             filtra_contar_por_parametros 
