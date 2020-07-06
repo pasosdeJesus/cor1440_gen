78f195a5a51b0cd6ab660249c781671e26e3ec53
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-07-06 06:24:29 -0500

    sin byebug

--- app/controllers/sip/personas_controller.rb
+++ app/controllers/sip/personas_controller.rb
@@ -3,7 +3,6 @@ require_dependency "cor1440_gen/concerns/controllers/personas_controller"
 
 module Sip
   class PersonasController < Heb412Gen::ModelosController
-    byebug
     include Cor1440Gen::Concerns::Controllers::PersonasController
   end
 end
