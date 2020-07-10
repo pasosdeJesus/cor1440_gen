d4e815058c2077d70920326bc5381a80cc3b14e7
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-07-10 10:54:24 -0500

    mejora migracion

--- db/migrate/20200229005951_fehayrespnonula.rb
+++ db/migrate/20200229005951_fehayrespnonula.rb
@@ -1,8 +1,9 @@
 class Fehayrespnonula < ActiveRecord::Migration[6.0]
   def up
+    primus = Usuario.all.order(:id)[0].id
     execute <<-SQL
       UPDATE cor1440_gen_actividad SET fecha='2020-12-31' where fecha IS NULL;
-      UPDATE cor1440_gen_actividad SET usuario_id=1 where usuario_id IS NULL;
+      UPDATE cor1440_gen_actividad SET usuario_id=#{primus} where usuario_id IS NULL;
     SQL
     change_column_null :cor1440_gen_actividad, :fecha, false
     change_column_null :cor1440_gen_actividad, :usuario_id, false
