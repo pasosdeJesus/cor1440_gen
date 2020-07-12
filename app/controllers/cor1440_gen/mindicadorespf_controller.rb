010cbfa30e68de1a94ba180eaeb8d371dbad342d
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-07-11 22:14:41 -0500

    Actualiza

--- app/controllers/cor1440_gen/mindicadorespf_controller.rb
+++ app/controllers/cor1440_gen/mindicadorespf_controller.rb
@@ -5,6 +5,10 @@ require_dependency 'cor1440_gen/concerns/controllers/mindicadorespf_controller'
 module Cor1440Gen
   class MindicadorespfController < Sip::ModelosController
 
+    before_action :set_mindicadorpf, 
+      only: [:show, :edit, :update, :destroy]
+    load_and_authorize_resource class: Cor1440Gen::Mindicadorpf
+
     include Cor1440Gen::Concerns::Controllers::MindicadorespfController
 
   end
