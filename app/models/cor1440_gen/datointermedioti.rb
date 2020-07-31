3628fc73abf4d41f46f51e940793e00ecec4a9f2
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-07-31 17:20:03 -0500

    Modularidad y validaciones

--- app/models/cor1440_gen/datointermedioti.rb
+++ app/models/cor1440_gen/datointermedioti.rb
@@ -1,15 +1,9 @@
 # encoding: UTF-8
 
+require 'cor1440_gen/concerns/models/datointermedioti'
+
 module Cor1440Gen
   class Datointermedioti < ActiveRecord::Base
-
-    belongs_to :tipoindicador, class_name: 'Cor1440Gen::Tipoindicador',
-      foreign_key: 'tipoindicador_id'
-
-    validates :nombre, length: { maximum: 1024}
-
-    def presenta_nombre
-      nombre
-    end
+    include Cor1440Gen::Concerns::Models::Datointermedioti
   end
 end
