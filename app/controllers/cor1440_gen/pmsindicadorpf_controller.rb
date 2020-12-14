e8070c73a73502b436e23d4c3e921679d804fc93
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-12-14 08:57:20 -0500

    Usa archivo en concerns

--- app/controllers/cor1440_gen/pmsindicadorpf_controller.rb
+++ app/controllers/cor1440_gen/pmsindicadorpf_controller.rb
@@ -1,72 +1,14 @@
-# encoding: UTF-8
+require_dependency "cor1440_gen/concerns/controllers/pmsindicadorpf_controller"
 
 module Cor1440Gen
-  class PmsindicadorpfController < ApplicationController
+  class PmsindicadorpfController < Heb412Gen::ModelosController
+    include Cor1440Gen::Concerns::Controllers::PmsindicadorpfController
 
+    #before_action :set_pmsindicadorpf,
+    #  only: [:show, :edit, :update, :destroy]      
 
-    def self.crea_pmindicadorpf(mind, fini, ffin, restiempo, meta)
-      pm = Cor1440Gen::Pmindicadorpf.create(
-        mindicadorpf_id: mind.id,
-        finicio: fini,
-        ffin: ffin,
-        restiempo: restiempo,
-        meta: meta)
-      pm.save(validate: false)
-      if mind.indicadorpf.tipoindicador && 
-          mind.indicadorpf.tipoindicador.datointermedioti 
-        dids = mind.indicadorpf.tipoindicador.datointermedioti.
-          map(&:id)
-        dids.each do |di|
-          dp = Cor1440Gen::DatointermediotiPmindicadorpf.create(
-            pmindicadorpf_id: pm.id,
-            datointermedioti_id: di
-          )
-          dp.save(validate: false)
-        end
-      end
-      return pm
-    end
+    #load_and_authorize_resource  class: Cor1440Gen::Pmsindicadorpf,
+    #  only: [:new, :create, :destroy, :edit, :update, :index, :show, :objetivospf]
 
-    # GET /pmsindicadorpf/new
-    def new
-      if params[:mindicadorpf_id] && Cor1440Gen::Mindicadorpf.where(id: params[:mindicadorpf_id].to_i).count == 1
-        mind = Cor1440Gen::Mindicadorpf.find(params[:mindicadorpf_id].to_i)
-        fini = Date.today
-        ffin = fini + 1.month
-        restiempo = 'Mes'
-        meta = 1
-        @pmindicadorpf = Cor1440Gen::Pmindicadorpf.create(
-          mindicadorpf_id: mind.id,
-          finicio: fini,
-          ffin: ffin,
-          restiempo: restiempo,
-          meta: meta)
-        if @pmindicadorpf.save
-          respond_to do |format|
-            format.js { render text: @pmindicadorpf.id.to_s }
-            format.json { render json: @pmindicadorpf.id.to_s, status: :created }
-            format.html { render inline: 'No implementado', 
-                          status: :unprocessable_entity }
-          end
-        else
-          render inline: 'No implementado', status: :unprocessable_entity 
-        end
-      else
-        render inline: 'Faltó identificación de mindicadorpf', 
-          status: :unprocessable_entity 
-      end
-    end
-
-    def destroy
-      if params[:id]
-        @pmindicadorpf = Cor1440Gen::Pmindicadorpf.find(params[:id])
-        @pmindicadorpf.destroy
-        respond_to do |format|
-          format.html { render inline: 'No implementado', 
-                        status: :unprocessable_entity }
-          format.json { head :no_content }
-        end
-      end
-    end
   end
 end
