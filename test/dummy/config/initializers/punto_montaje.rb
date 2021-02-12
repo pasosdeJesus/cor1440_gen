77bd21c0ae548bc8dfeb2e2c57f8e089aebf5fb5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-02-11 19:38:53 -0500

    Actualiza

--- test/dummy/config/initializers/punto_montaje.rb
+++ test/dummy/config/initializers/punto_montaje.rb
@@ -1,3 +1,5 @@
-Dummy::Application.config.relative_url_root = '/cor1440'
-Dummy::Application.config.assets.prefix = '/cor1440/assets/'
+Dummy::Application.config.relative_url_root = ENV.fetch(
+  'RUTA_RELATIVA', '/cor1440')
+Dummy::Application.config.assets.prefix = ENV.fetch(
+  'RUTA_RELATIVA', '/cor1440') + '/assets'
 
