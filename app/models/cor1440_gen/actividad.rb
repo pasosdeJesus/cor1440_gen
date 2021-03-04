9abc6bf717d6816f5ffba170f85f2003c9f5ebe6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-03-04 10:26:46 -0500

    indenta

--- app/models/cor1440_gen/actividad.rb
+++ app/models/cor1440_gen/actividad.rb
@@ -1,9 +1,7 @@
-# encoding: UTF-8
-
 require 'cor1440_gen/concerns/models/actividad'
 
 module Cor1440Gen
   class Actividad < ActiveRecord::Base
-        include Cor1440Gen::Concerns::Models::Actividad
+    include Cor1440Gen::Concerns::Models::Actividad
   end
 end
