c1d5f5336f2faa1c9999b60f3de5989544c7807d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-04-19 05:10:25 -0500

    Sector actor se pasó a sip en 2018

--- app/models/cor1440_gen/sectoractor.rb
+++
@@ -1,9 +0,0 @@
-# encoding: UTF-8
-
-require 'cor1440_gen/concerns/models/sectoractor'
-
-module Cor1440Gen
-  class Sectoractor < ActiveRecord::Base
-    include Cor1440Gen::Concerns::Models::Sectoractor
-  end
-end
