97effa4a933cd11b91c91e57416e1ce4cb3bf9ab
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-04-19 05:15:38 -0500

    Tipo

--- lib/cor1440_gen/concerns/models/actividadarea.rb
+++ lib/cor1440_gen/concerns/models/actividadarea.rb
@@ -14,7 +14,7 @@ module Cor1440Gen
 
           has_many :actividadareas_actividad, :dependent => :delete_all,
             class_name: 'Cor1440Gen::ActividadareasActividad'
-          has_many :actividad, :through => :actividadareas_activid,
+          has_many :actividad, :through => :actividadareas_actividad,
             class_name: 'Cor1440Gen::Actividad'
         
         end
