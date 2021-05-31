c9d68cc249c46ecd96739473de820d43e73132a0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-31 11:06:25 -0500

    Propuesta de sintaxis y parte de interpreteacion de lenguaje para medir indicadores

--- app/helpers/cor1440_gen/medicion_helper.rb
+++ app/helpers/cor1440_gen/medicion_helper.rb
@@ -2,9 +2,13 @@ module Cor1440Gen
   module MedicionHelper
 
 
-    # Referencia sintaxis de Excel: https://fenia266781730.files.wordpress.com/2019/04/smrdoc.pdf
-
-    # Basado en ejemplo de https://kschiess.github.io/parslet/get-started.html
+    # SINTAXIS. Basada en ejemplos:
+    #   * https://kschiess.github.io/parslet/get-started.html
+    #   * https://github.com/kschiess/parslet/blob/master/example/calc.rb
+    #   * https://github.com/kschiess/parslet/blob/master/example/boolean_algebra.rb
+    #   * https://en.wikipedia.org/wiki/Operator-precedence_parser
+    # Referencia sintaxis de Excel: 
+    #   * https://fenia266781730.files.wordpress.com/2019/04/smrdoc.pdf
     class ExpMed < Parslet::Parser
       # Reglas de un caracter
       rule(:pareni)     { str('(') >> espacio? }
@@ -14,77 +18,293 @@ module Cor1440Gen
       rule(:espacio)   { match('\s').repeat(1) }
       rule(:espacio?)  { espacio.maybe }
 
-      # elementos de una expresión
-      rule(:numero)         { match('[0-9.]').repeat(1).as(:num) >> espacio? }
-      rule(:iden)  { match('[a-z_.]').repeat(1) }
-      rule(:operador)       { match('[-+*/]') >> espacio? }
+      # Números positivos
+      rule(:entero) { 
+        match('[0-9]').repeat(1).as(:num) >> espacio? 
+      }
+      rule(:flotante) { 
+        (match('[0-9]').repeat(1) >> str(',') >> 
+         match(['0-9']).repeat(1)).as(:num) >> espacio? 
+      }
 
-      rule(:expmin) { numero | iden }
+      # Nombres
+      rule(:ident) { 
+        match('[A-Za-z_ÁÉÍÓÚÜÑáéíóúüñ]') >>
+        match('[A-Za-z_ÁÉÍÓÚÜÑáéíóúüñ0-9]').repeat(0)
+      }
 
-      # Partes de gramática
-      rule(:opbin)        { 
-        expmin.as(:izq) >> operador.as(:op) >> expresion.as(:der) }
+      # Aplicación funcional
       rule(:listaarg)    { expresion >> (coma >> expresion).repeat }
-      rule(:funcion)    { 
-        iden.as(:funcion) >> pareni >> listaarg.as(:listaarg) >> parend }
-      rule(:identificador)    { 
-        iden.as(:identificador) }
+      rule(:apfuncion)    { 
+        ident.as(:apfuncion) >> espacio? >> pareni >> 
+        listaarg.as(:listaarg) >> parend 
+      }
 
-      rule(:expresion) { funcion | opbin | numero | identificador }
+      # Primario para tener en cuenta paréntesis y -
+      rule(:expprimaria) {
+        #(pareni >> expresion >> parend >> str('.') >> 
+        #  ident.as(:campo)).as(:proyeccion) | 
+        pareni >> expresion >> parend | 
+        #(apfuncion >> str('.') >> ident.as(:campo)).as(:proyeccion) |
+        apfuncion |
+        #(ident.as(:registro) >> str('.') >> ident.as(:campo)).as(:proyeccion) |
+        ident.as(:identificador) >> espacio? |
+        flotante |
+        entero |
+        (str("-") >> espacio? >> expprimaria.as(:exp)).as(:menos)
+      }
+
+      rule(:exppro) {
+        (expprimaria.as(:registro) >> str('.') >> espacio? >> 
+         ident.as(:campo)).as(:proyeccion) |
+        expprimaria 
+      }
+
+      # Operaciones binarias
+      rule(:opmult) { match['*/'].as(:o) >> espacio? }
+      rule(:opadit) { match['+-'].as(:o) >> espacio? }
+
+      rule(:expmultiplicativa) {
+        (exppro.as(:izq) >> opmult >> 
+         expmultiplicativa.as(:der)).as(:opbin) | 
+        exppro
+      }
+
+      rule(:expaditiva) {
+        (expmultiplicativa.as(:izq) >> opadit >> 
+         expaditiva.as(:der)).as(:opbin) | 
+        expmultiplicativa
+      }
+     
+      rule(:expresion) {
+        espacio? >> expaditiva
+      }
+
       root :expresion
     end
 
-    LitEnt = Struct.new(:num) do
-      def eval; num.to_f; end
-    end
-    Suma = Struct.new(:izq, :der) do
-      def eval; izq.eval + der.eval; end
-    end
-    Resta = Struct.new(:izq, :der) do
-      def eval; izq.eval - der.eval; end
-    end
-    Multiplicacion = Struct.new(:izq, :der) do
-      def eval; izq.eval * der.eval; end
-    end
-    Division = Struct.new(:izq, :der) do
-      def eval; der.eval == 0 ? 0 : izq.eval / der.eval; end
-    end
-    LlamadFunl = Struct.new(:name, :args) do
-      def eval; p args.map { |s| s.eval }; end
+
+    # ÁRBOL DE SINTAXIS ABSTRACTA Y SU EVALUACIÓN
+    ###########################################
+
+    LitNum= Struct.new(:num) do
+      def eval(contexto); num.to_s.sub(',', '.').to_f; end
     end
 
+
+    Menos = Struct.new(:e) do
+      def eval(contexto); 
+        -(e.eval(contexto).to_f); end
+    end
+
+    Ident = Struct.new(:id) do
+      def eval(contexto)
+        if !contexto
+          STDERR.puts "** No se definió contexto"
+          return nil
+        end
+        if !contexto[id.to_s] && !contexto[id.to_s.to_sym]
+          STDERR.puts "** No se definió #{id.to_s} en contexto"
+          return nil
+        end
+        return contexto[id.to_s] || contexto[id.to_s.to_sym]
+      end
+    end
+
+    ApFun = Struct.new(:fun, :argsp) do
+      def eval(contexto)
+        # args que siempre sea vector con argumentos
+        args = argsp.class.to_s != 'Array' ? [argsp] : argsp
+        # argsev contendrá evaluaciónd de los argumentos necesarios
+        argsev = []
+        case fun.to_s.localize.casefold.to_s
+
+        when 'aplana'
+          if args.count != 1
+            STDERR.puts "** Función #{fun.to_s} requiere un parámetro"
+            return nil
+          end
+          argsev[0] = args[0].eval(contexto)
+          if !argsev[0].respond_to?(:count)
+            STDERR.puts "** Parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+          return argsev[0].flatten
+
+
+        when 'cuenta'
+          if args.count != 1
+            STDERR.puts "** Función #{fun.to_s} requiere un parámetro"
+            return nil
+          end
+          argsev[0] = args[0].eval(contexto)
+          if !argsev[0].respond_to?(:count)
+            STDERR.puts "** Parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+          return argsev[0].count
+
+        when 'interseccion', 'intersección'
+          if args.count != 2
+            STDERR.puts "** Función #{fun.to_s} requiere dos parámetros"
+            return nil
+          end
+          argsev = args.map{|a| a.eval(contexto)}
+          if !argsev[0].respond_to?(:count)
+            STDERR.puts "** Primer parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+          if !argsev[0].respond_to?(:count)
+            STDERR.puts "** Segundo parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+
+          return argsev[0] && argsev[1] ? argsev[0] & argsev[1] : nil
+
+
+        when 'mapeaproy'
+          if args.count != 2
+            STDERR.puts "** Función #{fun.to_s} requiere dos parámetro"
+            return nil
+          end
+          argsev[0] = args[0].eval(contexto)
+          if !argsev[0].respond_to?(:count)
+            STDERR.puts "** Primer parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+          if args[1].class != Ident
+            STDERR.puts "** Segundo parámetro de función #{fun.to_s} debe ser identificador"
+            return nil
+          end
+          argsev[1] = args[1].id.to_s
+          return argsev[0].map{|e|
+            e[argsev[1].to_sym] || e[argsev[1].to_s] 
+          }
+
+        when 'primera', 'primer', 'primero'
+          if args.count != 1
+            STDERR.puts "** Función #{fun.to_s} requiere un parámetro"
+            return nil
+          end
+          argsev[0] = args[0].eval(contexto)
+          if !argsev[0].respond_to?(:count)
+            STDERR.puts "** Parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+          if !argsev[0].count == 0
+            STDERR.puts "** Parámetro de función #{fun.to_s} es vector vacío"
+            return nil
+          end
+          return argsev[0][0]
+
+        when 'suma'
+          if args.count != 1
+            STDERR.puts "** Función #{fun.to_s} requiere un parámetro"
+            return nil
+          end
+          argsev[0] = args[0].eval(contexto)
+          if !argsev[0].respond_to?(:count)
+            STDERR.puts "** Primer parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+          return argsev[0].inject(0){|memo, e| 
+            memo + e.to_f
+          }
+
+
+        when 'únicos', 'unicos', 'únicas', 'unicas'
+          if args.count != 1
+            STDERR.puts "** Función #{fun.to_s} requiere un parámetro"
+            return nil
+          end
+          argsev[0] = args[0].eval(contexto)
+          if !argsev[0].respond_to?(:uniq)
+            STDERR.puts "** Parámetro de función #{fun.to_s} no es vector"
+            return nil
+          end
+          return argsev[0].uniq
+         # {|e|
+         #   if r.class != Array && r.class != Integer && r.class != Float &&
+         #       r.class != Date
+         #     r = e['id'] || e[:id]
+         #   else
+         #     r = e
+         #   end
+         #   return r
+         # }
+
+        end
+      end
+    end
+
+
+    Proy = Struct.new(:registro, :campo) do
+      def eval(contexto)
+        r = registro.eval(contexto)
+        c = campo.to_s
+        if r && (r[c] || r[c.to_sym])
+          return r[c] ? r[c] : r[c.to_sym]
+        else
+          STDERR.puts "** #{registro.to_s} no tiene campo #{c}"
+          return nil
+        end
+      end
+    end
+
+
+    OpBin = Struct.new(:izq, :op, :der) do
+      def eval(contexto)
+        eizq = izq.eval(contexto)
+        eder = der.eval(contexto)
+        case op
+        when '+'
+          return eizq + eder
+        when '-'
+          return eizq - eder
+        when '*'
+          return eizq * eder
+        when '/'
+          if eder == 0
+            STDERR.puts "** División entre 0"
+            return nil
+          end
+          return eizq / eder
+        else
+          STDERR.puts "** Operación '#{op}' desconocida"
+          return nil
+        end
+      end
+    end
+
+
+    # TRANSFORMACIÓN DE SINTAXIS CONCRETA A ÁRBOL DE SINTAXIS ABSTRACTA
     class ExpMedT < Parslet::Transform
-      rule(:num=> simple(:num))        { LitEnt.new(num) }
+      rule(:num => simple(:num))          { LitNum.new(num) }
+      rule(:identificador => simple(:id)) { Ident.new(id) }
+      rule(:menos => { :exp => subtree(:exp)})        { Menos.new(exp) }
       rule(
-        :izq => simple(:izq), 
-        :der => simple(:der), 
-        :op => '+')                     { Suma.new(izq, der) }
+        :apfuncion => simple(:fun), 
+        :listaarg => subtree(:listaarg))  { ApFun.new(fun, listaarg) }
       rule(
-        :izq => simple(:izq), 
-        :der => simple(:der), 
-        :op => '-')                     { Resta.new(izq, der) }
+        :proyeccion => { 
+          :registro=> subtree(:registro),
+          :campo => simple(:campo) } )    {Proy.new(registro, campo)}
       rule(
-        :izq => simple(:izq), 
-        :der => simple(:der), 
-        :op => '*')                     { Multiplicacion.new(izq, der) }
-      rule(
-        :izq => simple(:izq), 
-        :der => simple(:der), 
-        :op => '/')                     { Division.new(izq, der) }
-      rule(
-        :funcion => 'puts', 
-        :listaarg => subtree(:listaarg))  { LlamadFunl.new('puts', listaarg) }
+        :opbin => {
+          :izq => subtree(:izq), 
+          :o => simple(:op),
+          :der => subtree(:der) } )       { OpBin.new(izq, op, der) }
     end
 
 
     def revisa_sintaxis_expresion(e)
       reconocedor = ExpMed.new
-      begin
-        p = reconocedor.parse(e)
-      rescue
-        return false
-      end
+      p = reconocedor.parse(e)
       return true
+    rescue Parslet::ParseFailed => falla
+      STDERR.puts "** #{e}"
+      STDERR.puts falla.parse_failure_cause.ascii_tree
+      return false
     end
     module_function :revisa_sintaxis_expresion
 
@@ -95,15 +315,19 @@ module Cor1440Gen
     module_function :revisa_sintaxis_filtro
 
 
-    def evalua_expresion(e)
-      reconocedor = ExpMed.new
-      p = reconocedor.parse(e)
-      transf = ExpMedT.new
-      asa = transf.apply(p)
-      r = asa.eval 
-      return r
+    def evalua_expresion_medicion(e, contexto)
+      r = ExpMed.new
+      p = r.parse(e)
+      t = ExpMedT.new
+      asa = t.apply(p)
+      res = asa.eval(contexto)
+      return res
+    rescue Parslet::ParseFailed => falla
+      STDERR.puts "** #{e}"
+      STDERR.puts falla.parse_failure_cause.ascii_tree
+      return false
     end
-    module_function :evalua_expresion
+    module_function :evalua_expresion_medicion
 
 
   end
---
+++ test/helpers/cor1440_gen/evaluacion_medicion_helper_test.rb
@@ -0,0 +1,260 @@
+# encoding: UTF-8
+
+require_relative '../../test_helper'
+require 'cor1440_gen/medicion_helper'
+
+module Cor1440Gen
+  class EvaluacionMedicionHelperTest < ActionView::TestCase
+
+    include MedicionHelper
+
+    # La evaluación requiere un contexto donde se 
+    # definen identificadores con datos
+    #
+    # Los datos que se manejan son:
+    #   1. Número (enteros y flotantes). Se representan en decimal con , 
+    #      para separar parte entera de parte decimal --notación de Colombia.
+    #   2. Registro de una tabla (o tupla) (por ejemplo una actividad 
+    #      que tiene propiedades numéricas como población, tipo cadena 
+    #      como nombre y tipo fecha como fecha).  Referenciado por un 
+    #      identificador (en singular si es escalar o registro o plural si 
+    #        es vector).
+    #      No tiene sintaxi en lenguaje, pero para ejemplos 
+    #      podemos representar una actividad con 
+    #      {fecha: '2021-10-10', id: 433, nombre: 'Taller'}
+    #   3. Vector (de un solo tipo, números, cadenas, fechas o registros de 
+    #      la misma tabla). Referenciado por un identificador. Notación
+    #      preferible en plural.
+    #      No tienen sintaxis en lenguaje, pero para ejemplos 
+    #      podemos representar un vector de números con
+    #      [1, 3, 4, 5] y de actividades con [{fecha:'2021-10-10', id:2}, 
+    #       {fecha: '2021-11-11', id: 3}]
+    #
+    # Identificadores (como notación se sugieren tablas y vectores 
+    #   comenzando en mayúscula y en plural), lo que representa un número,
+    #   fecha, cadena o escalar en minúscula y en singular
+    # :
+    #   1. Dato intermedio ya definido (e.g num_mujeres_lactantes)
+    #   2. Campo de un registro (o proyección) (e.g población en el contexto 
+    #     de una actividad a sería primera(Actividades).población).  
+    #     También incluye un 
+    #     formulario incrustado en el registro (e.g anticipo en 
+    #     primera(Actividades).anticipo)
+    #   3. Una tabla (e.g Actividades, Organizaciones, Personas, Proyectos)
+    #   4. Otros definidos en ruby, por lo menos:
+    #     * Actividades_contribuyentes Actividades que aportan a un indicador de
+    #       resultado
+    #     * Efectos_contribuyentes Actividades que aportan a un indicador de
+    #       efecto
+    test "números e identificadores" do
+      # números
+      assert_equal 1.0, evalua_expresion_medicion(' 1 ', nil)
+      assert_equal 1.0, evalua_expresion_medicion("\n1,0\t", nil)
+      assert_equal -2.0, evalua_expresion_medicion('-2', nil)
+      assert_equal -2.0, evalua_expresion_medicion("-2,0", nil)
+      # identificadores
+      assert_nil evalua_expresion_medicion("m", nil)
+      assert_equal 1.0, evalua_expresion_medicion("m", {'m' => 1.0})
+      assert_equal -1.0, evalua_expresion_medicion("-m", {'m' => 1.0})
+      # paréntesis
+      assert_equal 1.0, evalua_expresion_medicion("(m)", {'m' => 1.0})
+      assert_equal -1.0, evalua_expresion_medicion("-(m)", {'m' => 1.0})
+      assert_equal -1.0, evalua_expresion_medicion("(-m)", {'m' => 1.0})
+    end
+
+    test "aplicación funcional" do
+      # los nombres de funciones no son sensibles a capitalización 
+      # --por si prefieren SUMA y otras estilo Excel
+      #
+      # primera o primero o primer : vector -> elemento
+      # última o último o ÚLTIMA o ÚLTIMO: vector -> elemento
+      # cuenta o CUENTA: vector -> numero
+      # únicos o únicas o ÚNICOS o ÚNICAS: vector -> vector (por el
+      #   momento en registros chequea profundamente que sean iguales).
+      # suma o SUMA: vector de numeros -> numero
+      # mapeaproy o MAPEAPROY: vector de objetos, nombre propiedad -> vector 
+      # aplana o APLANA: vector de vectores de objetos -> vector de objetos
+      #   [[1,2], [3,4]] ->[1,2,3,4]
+      # intersección  retorna elementos comunes a 2 vectores sin repetición
+      #
+      # Función cuenta
+      assert_equal 0, evalua_expresion_medicion(
+        "cuenta(Actividades)", 
+        {'Actividades' => []}
+      )
+      assert_equal 1, evalua_expresion_medicion(
+        "cuenta(Actividades)", 
+        {'Actividades' => [1]}
+      )
+      # proyeccion
+      assert_equal 5, evalua_expresion_medicion(
+        "a.población", 
+        {'a' => {'población' => 5}}
+      )
+      # Función primera
+      assert_equal 5, evalua_expresion_medicion(
+        "primera(Actividades).población", 
+        {'Actividades' => [{'población' => 5}]}
+      )
+      assert_equal 6, evalua_expresion_medicion(
+        "PRIMERO(Actividades).población", 
+        {'Actividades' => [{'población' => 6}]}
+      )
+
+      # Función únicos
+      assert_equal 1, evalua_expresion_medicion(
+        'cuenta(únicos(Actividades))',
+        {'Actividades' => [1, 1]}
+      )
+      assert_equal 2, evalua_expresion_medicion(
+        'cuenta(únicos(Actividades))',
+        {'Actividades' => [{id:1, otro:2}, {id:1, otro: 3}]}
+      )
+
+      # Función mapeaproy
+      assert_equal [2,3], evalua_expresion_medicion(
+        'mapeaproy(Actividades, poblacion)',
+        {'Actividades' => [{poblacion: 2}, {poblacion: 3}]}
+      )
+
+      # Función suma
+      assert_equal 5, evalua_expresion_medicion(
+        'suma(mapeaproy(Actividades, poblacion))',
+        {'Actividades' => [{poblacion: 2}, {poblacion: 3}]}
+      )
+
+      # Función aplana
+      assert_equal [1,2,3,4], evalua_expresion_medicion(
+        'aplana(mapeaproy(Actividades, Asistentes))',
+        {'Actividades' => [{Asistentes: [1, 2]}, {Asistentes: [3, 4]}]}
+      )
+      assert_equal [1,2,2,1], evalua_expresion_medicion(
+        'aplana(mapeaproy(Actividades, Asistentes))',
+        {'Actividades' => [{Asistentes: [1, 2]}, {Asistentes: [2, 1]}]}
+      )
+
+      # Función intersección
+      assert_equal [2], evalua_expresion_medicion(
+        'intersección(Actividades1, Actividades2)',
+        {Actividades1: [1,2,2,3],
+         Actividades2: [2,4,2,6]}
+      )
+
+      # Cuenta asistentes
+      assert_equal 4, evalua_expresion_medicion(
+        'cuenta(aplana(mapeaproy(Actividades, Asistentes)))',
+        {'Actividades' => [{Asistentes: [1, 2]}, {Asistentes: [2, 1]}]}
+      )
+
+      # Cuenta asistentes únicos
+      assert_equal 2, evalua_expresion_medicion(
+        'cuenta(unicos(aplana(mapeaproy(Actividades, Asistentes))))',
+        {'Actividades' => [{Asistentes: [1, 2]}, {Asistentes: [2, 1]}]}
+      )
+
+      # Cuenta organizaciones
+      assert_equal 2, evalua_expresion_medicion(
+        'cuenta(aplana(mapeaproy(Actividades, Organizaciones)))',
+        {'Actividades' => [{Asistentes: [1, 2], Organizaciones: [5]}, 
+                           {Asistentes: [2, 1], Organizaciones: [5]}]}
+      )
+      # Cuenta organizaciones únicas
+      assert_equal 1, evalua_expresion_medicion(
+        'cuenta(unicas(aplana(mapeaproy(Actividades, Organizaciones))))',
+        {'Actividades' => [{Asistentes: [1, 2], Organizaciones: [5]}, 
+                           {Asistentes: [2, 1], Organizaciones: [5]}]}
+      )
+
+    end
+
+    # Operaciones binarias y paréntesis
+    test "binarias y paréntesis" do
+      assert_equal 1, evalua_expresion_medicion('(1)', nil)
+      assert_equal 2, evalua_expresion_medicion('(e)', {'e' => 2})
+      assert_equal 3, evalua_expresion_medicion('1+2', nil)
+      assert_equal 2, evalua_expresion_medicion('1*2', nil)
+      assert_equal 0.5, evalua_expresion_medicion('1/2', nil)
+      assert_equal -1, evalua_expresion_medicion('1-2', nil)
+      assert_equal 2, evalua_expresion_medicion('1+x', {'x' => 1})
+      assert_equal 2, evalua_expresion_medicion('x*100/50', {'x' => 1})
+      assert_equal 2, evalua_expresion_medicion('x*(100/50)', {'x' => 1})
+    end
+
+
+    test "trienal_cinep_2021" do
+      # O1I1, efecto, meta 20
+      assert_equal 3, evalua_expresion_medicion(
+        'cuenta(Efectos_contribuyentes)',
+        {Efectos_contribuyentes: [{}, {}, {}]}
+      )
+
+      # O1I2, efecto, meta 90
+      con = {
+        Efectos_contribuyentes: [
+          {Organizaciones: [{id: 1}, {id: 2}]}, 
+          {Organizaciones: [{id: 2}, {id: 3}]}
+        ],
+        Escuelas_rurales_lineabase_2021: [
+          {id: 2}, {id: 3}, {id: 4}, {id: 5}
+        ]
+      }
+
+      assert_equal [ [{id: 1}, {id: 2}], [{id: 2}, {id: 3}] ],
+        evalua_expresion_medicion(
+          'mapeaproy(Efectos_contribuyentes, Organizaciones)', con
+      )
+      assert_equal [ {id: 1}, {id: 2}, {id: 2}, {id: 3} ],
+        evalua_expresion_medicion(
+        'aplana(mapeaproy(Efectos_contribuyentes, Organizaciones))', con)
+
+      assert_equal [ {id: 2}, {id: 3} ],
+        evalua_expresion_medicion(
+        'intersección(
+          aplana(mapeaproy(Efectos_contribuyentes, Organizaciones)), 
+          Escuelas_rurales_lineabase_2021)', con)
+
+
+      assert_equal 50, evalua_expresion_medicion(
+        'cuenta(
+          intersección(
+            aplana(mapeaproy(Efectos_contribuyentes, Organizaciones)), 
+            Escuelas_rurales_lineabase_2021
+          )
+         )*100/cuenta(Escuelas_rurales_lineabase_2021)', con)
+
+      # O2I1, resultado, meta 5
+       assert_equal 3, evalua_expresion_medicion(
+        'cuenta(Actividades_contribuyentes)',
+        {Actividades_contribuyentes: [{id: 1}, {id: 2}, {id: 3}]}
+       )
+
+      # O2I2, efecto, meta 30
+      con = {
+        Efectos_contribuyentes:[
+          {Organizaciones: [{id: 1}]},
+          {Organizaciones: [{id: 2}, {id: 1}]}
+        ]
+      }
+      assert_equal 2, evalua_expresion_medicion(
+        'cuenta(
+          únicas(
+            mapeaproy(Efectos_contribuyentes, Organizaciones)
+          )
+        )', con
+      )
+
+      # O3I1, efecto, meta 6
+      assert_equal true, revisa_sintaxis_expresion(
+        'cuenta(efectos_contribuyentes)'
+      )
+
+      # O3I2, efecto, meta 60
+      assert_equal true, revisa_sintaxis_expresion(
+        'cuenta(unicas(listablanca(mapa(efectos_contribuyentes, organizacion), 
+          municipios_pdet_linebase_2021)))*100/cuenta(municipios_pdet_lineabase_2021)'
+      )
+    end
+
+
+  end  # class
+end    # module
