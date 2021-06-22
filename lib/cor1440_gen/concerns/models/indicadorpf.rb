24d45eb691a259e5fa8c3cb08b631aefbdc524e8
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-06-21 20:35:04 -0500

    completa modelo

--- lib/cor1440_gen/concerns/models/indicadorpf.rb
+++ lib/cor1440_gen/concerns/models/indicadorpf.rb
@@ -21,6 +21,12 @@ module Cor1440Gen
             class_name: 'Cor1440Gen::Tipoindicador',
             foreign_key: 'tipoindicador_id', optional: true
 
+          has_many :efecto, 
+            foreign_key: 'indicadorpf_id', 
+            validate: true,
+            dependent: :destroy, 
+            class_name: 'Cor1440Gen::Efecto'
+
           has_many :mindicador, 
             foreign_key: 'indicadorpf_id', 
             validate: true,
