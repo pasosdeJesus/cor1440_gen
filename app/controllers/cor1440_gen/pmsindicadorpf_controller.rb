bdd6d494633ad16701e89781b3d1ff81f30a4a49
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-10-28 10:34:27 -0500

    nombre

--- app/controllers/cor1440_gen/pmsindicadorpf_controller.rb
+++ app/controllers/cor1440_gen/pmsindicadorpf_controller.rb
@@ -3,7 +3,7 @@ require_dependency "cor1440_gen/concerns/controllers/pmsindicadorpf_controller"
 module Cor1440Gen
   class PmsindicadorpfController < Heb412Gen::ModelosController
 
-    load_and_authorize_resource class: Cor1440Gen::Pmindicador
+    load_and_authorize_resource class: Cor1440Gen::Pmindicadorpf
     include Cor1440Gen::Concerns::Controllers::PmsindicadorpfController
 
   end
