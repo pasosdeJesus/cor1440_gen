428fa51f6ac034e94de67f6d495c2b6c455963fb
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-11-01 17:18:15 -0500

    Actualiza

--- test/dummy/app/packs/entrypoints/application.js
+++ test/dummy/app/packs/entrypoints/application.js
@@ -1,45 +1,46 @@
 /* eslint no-console:0 */
-// Este archivo se compila automaticamente con Webpack, junto con otros
+
+// Este archivo de compila automaticamente con Webpack, junto con otros
 // archivos presentes en este directorio.  Lo animamos a poner la lógica
-// de su aplicacíon en una estructura relevante dentro de app/javascript
-// y solo usar estos archivos de paquete para referencia ese código
-// para que sea compilado.
+// de su aplicación en una estructura relevante en app/javascript y usar
+// solo estos archivos pack para referenciar ese código de manera que sea
+// compilado.
 //
-// Para referenciar este archivo, añada <%= javascript_pack_tag 'application' %>
-// al archivo de maquetación adecuado, como 
+// Para referenciar este archivo agregue 
+// <%= javascript_pack_tag 'application' %> 
+// en el archivo de maquetación adecuado, como 
 // app/views/layouts/application.html.erb
 
 
-// Quite comentario para copiar todas las imágenes estáticas de ../images 
-// a la carpeta de salida y referenciarlas con la función auxiliar
-// image_pack_tag helper en las vistas (e.g <%= image_pack_tag 'rails.png' %>)
-// o mediante el auxiliar de Javascript `imagePath`.
+// Quite el comentario para copiar todas las imágenes estáticas de
+// ../images en la carpeta de salida y referencielas con el auxiliar
+// image_pack_tag en las vistas (e.g <%= image_pack_tag 'rails.png' %>)
+// o con el siguiente auxiliar `imagePath`:
 //
-// const imagenes = require.context('../images', true)
-// const rutaImagenes = (nombre) => images(nombre, true)
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
 
 console.log('Hola Mundo desde Webpacker')
 
 import Rails from "@rails/ujs"
 Rails.start()
 
-  import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 Turbolinks.start()
 
-import ApexCharts from 'apexcharts'
-window.ApexCharts = ApexCharts
-
 import $ from "expose-loader?exposes=$,jQuery!jquery";
+import 'jquery-ui'
+import 'jquery-ui/ui/widgets/autocomplete'
+import 'jquery-ui/ui/focusable'
+import 'jquery-ui/ui/data'
+import 'jquery-ui/ui/widgets/tooltip'
 
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
-
-import * as bootstrap from 'bootstrap'
-
+import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
-import 'jquery-ui'
-import 'jquery-ui/ui/widgets/autocomplete' 
-import 'jquery-ui/ui/data' 
-import 'jquery-ui/ui/focusable' 
+
+import ApexCharts from 'apexcharts'
+window.ApexCharts = ApexCharts
 
