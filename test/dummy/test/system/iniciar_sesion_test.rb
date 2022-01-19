cad878be8376ec386173d1241c1c65ffd5eef3cd
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-01-19 12:32:30 -0500

    Actualiza

--- test/dummy/test/system/iniciar_sesion_test.rb
+++ test/dummy/test/system/iniciar_sesion_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class IniciarSesionTest < ApplicationSystemTestCase
 
   test "iniciar sesión" do
-    Sip::CapybaraHelper.iniciar_sesion(self, root_path, 'sip', 'sip')
+    Sip::CapybaraHelper.iniciar_sesion(self, root_path, 'cor1440', 'cor1440')
   end
 
 end
