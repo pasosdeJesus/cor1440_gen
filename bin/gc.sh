e88ed21d0d4a11aa04fcab6688bac0638880bf27
Nombre Usuario <correo@usuario.net>
2022-02-18 12:13:42 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
-# Revisa errore comunes, ejecuta pruebas de regresión y del sistema y envia a github 
+# Actualiza dependencias, revisa errores comunes, 
+# ejecuta pruebas y envía a repositorio
 
-function cableado {
-	for n in $*; do 
-		echo "Revisando $n"
-		grep "^ *gem *.${n}.*, *path:" Gemfile > /dev/null 2> /dev/null
-		if (test "$?" = "0") then {
-			echo "Gemfile incluye un ${n} cableado al sistema de archivos"
-			exit 1;
-		} fi;
-	done
-}
+if (test -f ".env") then {
+  rutaap="./"
+} elif (test -f "test/dummy/.env") then {
+  rutaap="test/dummy"
+} else {
+  echo "No se determino ruta de aplicación. Falta archivo .env"
+  exit 1;
+} fi;
 
-d=`grep "gem.*pasosdeJesus" Gemfile | sed -e "s/.*gem ['\"]//g;s/['\"].*//g"`
-cableado $d
+echo "Ruta de la aplicación: $rutaap"
+
+s=`grep -B 1 "^ *path" Gemfile 2> /dev/null`
+if (test "$?" = "0") then {
+  echo "Gemfile incluye gema cableada al sistema de archivos ($s)"
+  exit 1;
+} fi;
 
 grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
@@ -31,7 +35,7 @@ if (test "$SINAC" != "1") then {
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
-	(cd test/dummy; CXX=c++ yarn upgrade)
+	(cd $rutaap; CXX=c++ yarn upgrade)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
@@ -42,26 +46,20 @@ if (test "$SININS" != "1") then {
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
-	(cd test/dummy; CXX=c++ yarn install)
+	(cd $rutaap; CXX=c++ yarn install; bin/rails assets:precompile)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
 
 if (test "$SINMIG" != "1") then {
-
-(cd test/dummy; CONFIG_HOSTS=127.0.0.1 bin/rails test:system)
-if (test "$?" != "0") then {
-	echo "No pasaron pruebas del sistema";
-	exit 1;
-} fi;
-	(cd test/dummy; bin/rails db:migrate sip:indices db:schema:dump)
+	(cd $rutaap; bin/rails db:migrate sip:indices db:schema:dump)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
 
-(cd test/dummy; RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
+(cd $rutaap; RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
 if (test "$?" != "0") then {
 	echo "No puede preparse base de prueba";
 	exit 1;
@@ -73,7 +71,14 @@ if (test "$?" != "0") then {
 	exit 1;
 } fi;
 
-(cd test/dummy; RAILS_ENV=test bin/rails db:schema:dump)
+(cd $rutaap; CONFIG_HOSTS=127.0.0.1 bin/rails test:system)
+if (test "$?" != "0") then {
+	echo "No pasaron pruebas del sistema";
+	exit 1;
+} fi;
+
+
+(cd $rutaap; RAILS_ENV=test bin/rails db:schema:dump)
 
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
