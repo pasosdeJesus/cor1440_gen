1a96abafb6b8a5a8f93508873b05dc9e8722e42b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-10 23:27:28 -0500

    Refactoriza mas y crea Cor1440Gen::Proyectofinanciero::actividades_comunes_id y Cor1440Gen::Proyectofinancero::en_toda_actividad_id

--- lib/cor1440_gen/concerns/models/proyectofinanciero.rb
+++ lib/cor1440_gen/concerns/models/proyectofinanciero.rb
@@ -322,19 +322,168 @@ module Cor1440Gen
 
 
           def presenta(atr)
+
+            ## 4 Primeros Indicadores de objetivo
+            cindob = /indicadorobj(.*)$/.match(atr.to_s)
+            if cindob
+              indobs = indicadorobjetivo.where(resultadopf_id: nil).order(:id)
+              numero = cindob[1].split("_")[0]
+              campo = cindob[1].split("_")[1]
+              if indobs.count >= numero.to_i
+                indicadorob = indobs[numero.to_i-1]
+                if indicadorob
+                  case campo
+                  when 'refobj'
+                    return indicadorob.objetivopf ? indicadorob.objetivopf.numero : ''
+                  when 'codigo'
+                    return indicadorob.numero ? indicadorob.numero : ''
+                  when 'nombre'
+                    return indicadorob.indicador ? indicadorob.indicador : ''
+                  when 'tipo'
+                    return indicadorob.tipoindicador ? indicadorob.tipoindicador.nombre : ''
+                  end
+                end
+              else
+                return ''
+              end
+            end
+            ## 4 Primeros Resultados
+            cres = /resultado(.*)$/.match(atr.to_s)
+            if cres
+              resultados = resultadopf.order(:id)
+              numero = cres[1].split("_")[0]
+              campo = cres[1].split("_")[1]
+              if resultados.count >= numero.to_i
+                resultado = resultados[numero.to_i-1]
+                if resultado
+                  case campo
+                  when 'refobj'
+                    return resultado.objetivopf ? resultado.objetivopf.numero : ''
+                  when 'codigo'
+                    return resultado.numero ? resultado.numero : ''
+                  when 'resultado'
+                    return resultado.resultado ? resultado.resultado : ''
+                  end
+                end
+              else
+                return ''
+              end
+            end
+            ## 6 Primeros indicadores de resultados
+            cinres = /indicadorres(.*)$/.match(atr.to_s)
+            if cinres
+              indiresultados = indicadorpf.where(objetivopf_id: nil).order(:id)
+              numero = cinres[1].split("_")[0]
+              campo = cinres[1].split("_")[1]
+              if indiresultados.count >= numero.to_i
+                indicador = indiresultados[numero.to_i-1]
+                if indicador
+                  case campo
+                  when 'refres'
+                    ref = indicador.resultadopf
+                    return ref ? ref.numero + ref.objetivopf.numero : ''
+                  when 'codigo'
+                    return indicador.numero ? indicador.numero : ''
+                  when 'tipo'
+                    return indicador.tipoindicador ? indicador.tipoindicador.nombre : ''
+                  when 'indicador'
+                    return indicador.indicador ? indicador.indicador : ''
+                  end
+                end
+              else
+                return ''
+              end
+            end
+            ## 8 Primeras Actividades
+            cact = /actividadpf(.*)$/.match(atr.to_s)
+            if cact
+              actividades = actividadpf.order(:id)
+              numero = cact[1].split("_")[0]
+              campo = cact[1].split("_")[1]
+              if actividades.count >= numero.to_i
+                actividad = actividades[numero.to_i-1]
+                if actividad
+                  case campo
+                  when 'refresultado'
+                    ref = actividad.resultadopf
+                    return ref ? ref.numero + ref.objetivopf.numero : ''
+                  when 'codigo'
+                    return actividad.nombrecorto ? actividad.nombrecorto : ''
+                  when 'tipo'
+                    return actividad.actividadtipo ? actividad.actividadtipo.nombre : ''
+                  when 'actividad'
+                    return actividad.titulo ? actividad.titulo : ''
+                  when 'descripcion'
+                    return actividad.descripcion ? actividad.descripcion : ''
+                  when 'indicadoresgifmm'
+                    return actividad.indicadorgifmm ? actividad.indicadorgifmm.nombre : ''
+                  end
+                end
+              else
+                return ''
+              end
+            end
+            #Exporta Campos básicos de Convenios Financiados
             case atr.to_s
-            when 'proyectofinanciero_usuario', 'equipotrabajo'
+            when 'nombres'
+              nombre
+            when 'financiador'
+              financiador ? financiador.pluck(:nombre).join(',') : ''
+            when 'area'
+              proyecto ? proyecto.pluck(:nombre).join(',') : ''
+            when 'responsable'
+              responsable ? responsable.nusuario : ''
+            when 'equipotrabajo'
               self.proyectofinanciero_usuario.map {|pu|
                 pu.usuario ? pu.usuario.nusuario : 'Por contratar'
               }.join('; ')
+            when 'objetivos'
+              objetivopf ? objetivopf.pluck(:numero, :objetivo).map{|e| e.join(': ')}.join('. ') : ''
+            when 'obj1_texto'
+              objetivopf.order(:id)[0] ? objetivopf.order(:id)[0].objetivo : ''
+            when 'obj1_cod'
+              objetivopf.order(:id)[0] ? objetivopf.order(:id)[0].numero : ''
+            when 'obj2_texto'
+              objetivopf.order(:id)[1] ? objetivopf.order(:id)[1].objetivo : ''
+            when 'obj2_cod'
+              objetivopf.order(:id)[1] ? objetivopf.order(:id)[1].numero : ''
+            when 'indicadores_obj'
+              inds = indicadorpf.where(resultadopf_id: nil).pluck(:numero, :indicador, :tipoindicador_id, :objetivopf_id)
+              indicadores = inds.map{|indi| (indi[3] ? Cor1440Gen::Objetivopf.find(indi[3]).numero : '') + ':' + (indi[0] ? indi[0] : '') + ': ' + (indi[1] ? indi[1] : '') + ', ' + (indi[2] ? Cor1440Gen::Tipoindicador.find(indi[2]).nombre : '')}.join('. ')
+              indicadorobjetivo ? indicadores : ''
+            when 'indicadoresres'
+              indres = indicadorpf.where(objetivopf: nil).order(:id).pluck(:resultadopf_id, :numero, :indicador, :tipoindicador_id)
+              indicadores = indres.map{|indi| (indi[0] ? (Cor1440Gen::Resultadopf.find(indi[0]).objetivopf.numero + ':' + Cor1440Gen::Resultadopf.find(indi[0]).numero) : '') + '. ' + (indi[1] ? indi[1] : '') + ': ' + (indi[2] ? indi[2] : '') + ', ' + (indi[3] ? Cor1440Gen::Tipoindicador.find(indi[3]).nombre : '')}.join('. ')
+              indicadorpf.where(objetivopf: nil) ? indicadores : ''
+            when 'resultados'
+              ress = resultadopf.pluck(:numero, :objetivopf_id, :resultado)
+              resultados = ress.map{|res| (res[0] ? res[0] : '') + ': ' + (res[1] ? Cor1440Gen::Objetivopf.find(res[1]).numero : '') + ', ' + (res[2] ? res[2] : '')}.join('. ')
+              resultadopf ? resultados : ''
+            when 'actividadespf'
+              acts = actividadpf.order(:id).pluck(:resultadopf_id, :nombrecorto, :actividadtipo_id, :titulo, :descripcion, :indicadorgifmm_id)
+              actividades = acts.map{|act| (act[0] ? Cor1440Gen::Resultadopf.find(act[0]).objetivopf.numero : '') + (act[0] ? Cor1440Gen::Resultadopf.find(act[0]).numero : '') + ': ' + (act[1] ? act[1] : '') + ', ' + (act[2] ? Cor1440Gen::Actividadtipo.find(act[2]).nombre : '') + ', ' + (act[3] ? act[3] : '') + ', ' + (act[4] ? act[4] : '') + ', ' + (act[5] ? Indicadorgifmm.find(act[5]).nombre : '')}.join('. ')
             else
               presenta_gen(atr)
             end
+
+          end
+
+
+          # Id del proyecto con actividades comunes vigente
+          # nil significa que no hay.
+          def self.actividades_comunes_id
+            nil
+          end
+
+          # Id de un proyecto que deba agregarse siempre a toda actividad
+          # nil significa que no hay.
+          def self.en_toda_actividad_id
+            nil
           end
 
 
         end #included
-        
+
         class_methods do
 
           def human_attribute_name(atr, poromision = "")
