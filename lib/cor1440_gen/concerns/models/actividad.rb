a7940fc2c7906ccd1cf2daabb34b6dd373152324
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-12-28 22:22:00 -0500

    Intentando con after_save en lugar de after_commit para actualizar tabla de poblacion tras cambiar actividad

--- lib/cor1440_gen/concerns/models/actividad.rb
+++ lib/cor1440_gen/concerns/models/actividad.rb
@@ -244,7 +244,7 @@ module Cor1440Gen
             ConteosHelper.recalcula_poblacion(self)
           end
 
-          after_commit do |actividad|
+          after_save do |actividad|
             if !Rails.configuration.x.cor1440_edita_poblacion
               actividad.recalcula_poblacion
             end
