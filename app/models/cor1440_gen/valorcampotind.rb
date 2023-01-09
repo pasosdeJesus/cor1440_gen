f6d8873af21e6d1a31a451dcc32de82283a575db
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-09 18:32:37 -0500

    Quita valorcampotind

--- app/models/cor1440_gen/valorcampotind.rb
+++
@@ -1,7 +0,0 @@
-require 'cor1440_gen/concerns/models/valorcampotind'
-
-module Cor1440Gen
-  class Valorcampotind < ActiveRecord::Base
-    include Cor1440Gen::Concerns::Models::Valorcampotind
-  end
-end
