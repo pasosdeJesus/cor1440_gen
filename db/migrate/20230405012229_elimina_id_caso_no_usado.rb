d900a52f69d87684a0bad1948b69f5812a7e7fa7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-04-08 07:49:40 -0500

    Mejora migracion

--- db/migrate/20230405012229_elimina_id_caso_no_usado.rb
+++ db/migrate/20230405012229_elimina_id_caso_no_usado.rb
@@ -1,12 +1,13 @@
 class EliminaIdCasoNoUsado < ActiveRecord::Migration[7.0]
   def up
-    if (Msip::Ubicacion.where.not(id_caso: nil).count == 0) 
-      remove_column :msip_ubicacion, :id_caso, cascade: true
+    if (!table_exists?('sivel2_gen_caso'))
+      if Msip::Ubicacion.where.not(id_caso: nil).count == 0
+        remove_column :msip_ubicacion, :id_caso, cascade: true
+      end
+      if Msip::Ubicacion.where.not(caso_id: nil).count == 0 
+        remove_column :msip_ubicacion, :caso_id, cascade: true
+      end
     end
-    if (Msip::Ubicacion.where.not(caso_id: nil).count == 0) 
-      remove_column :msip_ubicacion, :caso_id, cascade: true
-    end
-
   end
 
   def down
