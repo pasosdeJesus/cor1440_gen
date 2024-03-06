ab7d1e2791b4815441704bf17485162a82c5a5db
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-03-06 10:55:26 -0500

    Actualiza

--- bin/regresion.sh
+++ bin/regresion.sh
@@ -54,10 +54,12 @@ if (test "$?" != "0") then {
 } fi;
 
 
-CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ bin/rails test `find test/integration -name "*rb" -type f`
-if (test "$?" != "0") then {
-  echo "No pasaron pruebas de integración";
-  exit 1;
+if (test -d test/integration) then {
+  CONFIG_HOSTS=www.example.com RUTA_RELATIVA=/ bin/rails test `find test/integration -name "*rb" -type f`
+  if (test "$?" != "0") then {
+    echo "No pasaron pruebas de integración";
+    exit 1;
+  } fi;
 } fi;
 
 echo "== Pruebas de regresión al sistema"
