fc13f87708ec3368aef9734a56a72197070901f0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-05-25 15:20:41 -0500

    Pasar de chosen a tom-select

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -14,9 +14,23 @@ import '../../vendor/assets/javascripts/jquery-ui'
 
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
 import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
-import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
+
+import Msip__Motor from "./controllers/msip/motor"
+window.Msip__Motor = Msip__Motor
+Msip__Motor.iniciar()  // Este se ejecuta una vez cuando se está cargando la aplicación tal vez antes que la página completa o los recursos
+
+import TomSelect from 'tom-select';
+window.TomSelect = TomSelect;
+window.configuracionTomSelect = {
+    create: false,
+    diacritics: true, //no sensitivo a acentos
+    sortField: {
+          field: "text",
+          direction: "asc"
+        }
+}
 
 import ApexCharts from 'apexcharts'
 window.ApexCharts = ApexCharts
@@ -47,6 +61,9 @@ promesaRecursosSprocketsYDocumento.then((mensaje) => {
   console.log('Cargando recursos sprockets')
   var root;
   root = window;
+
+  Msip__Motor.ejecutarAlCargarDocumentoYRecursos()  // Este se ejecuta cada vez que se carga una página que no está en cache y tipicamente después de que se ha cargado la página completa y los recursos
+
   root.cor1440_gen_activa_autocompleta_mismotipo = true
   msip_prepara_eventos_comunes(root);
   heb412_gen_prepara_eventos_comunes(root);
