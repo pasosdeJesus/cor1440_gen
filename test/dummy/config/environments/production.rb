9fa991e249523e5fbccddf7ffdca3196d8232b79
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-08-27 10:15:00 -0500

    Actualiza

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.public_file_server.enabled = false
 
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = nil
+  # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
