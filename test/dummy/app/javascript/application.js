1bc1cf9cb66abe194d5ca54e7ff4441cd8dcd75f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-27 15:36:17 -0500

    Punto de inicializaicon js. Avanza https://gitlab.com/pasosdeJesus/sivel2_gen/-/issues/707

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -36,7 +36,6 @@ window.Heb412Gen__Motor = Heb412Gen__Motor
 import Cor1440Gen__Motor from "./controllers/cor1440_gen/motor"
 window.Cor1440Gen__Motor = Cor1440Gen__Motor
 
-
 import ApexCharts from 'apexcharts'
 window.ApexCharts = ApexCharts
 
