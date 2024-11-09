913a80bb27358528b095f4029efb185793f12702
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-11-09 02:01:04 -0500

    Avanza doc. Avanza https://gitlab.com/pasosdeJesus/si_jrscol/-/issues/1084

--- app/models/cor1440_gen/proyecto_proyectofinanciero.rb
+++ app/models/cor1440_gen/proyecto_proyectofinanciero.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Cor1440Gen
+  # Relación n:n entre área de actividad y convenio financiado
   class ProyectoProyectofinanciero < ActiveRecord::Base
     belongs_to :proyecto,
       class_name: "Cor1440Gen::Proyecto",
